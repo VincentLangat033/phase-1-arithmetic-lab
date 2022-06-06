13c5395de4fe3115d588143905298c21832575d7
Vincent Langat <kimutailangat033@gmail.com>
2022-06-06 18:49:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+ 
+   let num1=62;
+   let num2=1;
+   const multiply= num1 * num2;
+   console.log(multiply);
+  let random = Math.floor(Math.random()*10);
+  console.log(random);
+  let num3=29;
+  let num4=5;
+  let mod=num3%num4;
+  console.log(mod);
+    let max=Math.max(2,4,6,18,20);
+    console.log(max)
+
