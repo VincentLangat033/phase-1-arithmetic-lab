527e5f6ab90dba0929921d173a69a1a179e12a47
Vincent Langat <kimutailangat033@gmail.com>
2022-06-07 00:18:40 +0300

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,11 @@
    let num2=1;
    const multiply= num1 * num2;
    console.log(multiply);
+  //  Alternative way to do it  is given below
+//   const multiply= function(num1, num2){
+//     console.log(num1*num2);
+// }
+// multiply(2,31)
   let random = Math.floor(Math.random()*10);
   console.log(random);
   let num3=29;
